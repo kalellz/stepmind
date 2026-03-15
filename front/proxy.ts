1ccfc1249a751664a81baedcdf3f4e45614b8432
Victor Santos <menezes.victorsantos@outlook.com>
2026-03-15 17:10:14 -0300

    fix: proxy auth management

--- front/proxy.ts
+++ front/proxy.ts
@@ -1,11 +1,18 @@
+import { getSessionCookie } from 'better-auth/cookies';
 import { NextResponse } from 'next/server'
 import { NextRequest } from "next/server";
 
 export default function proxy(req: NextRequest) {
-    return NextResponse.redirect(new URL('/login', req.url))
+    const sessionCookie = getSessionCookie(req)
+    console.log(sessionCookie)
+    const fakeLogin = false
+    if (!sessionCookie || fakeLogin) {
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+    return NextResponse.next()
 }
 
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.\.png$).)']
+    matcher: ['/']
 }
