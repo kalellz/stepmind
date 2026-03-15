af0797743f9cba47bb2b91048f9019fa8aabd4d2
Kalel Rodrigues <kalel.rodrigues@icloud.com>
2026-03-15 14:53:45 -0300

    feat: add change theme button

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,19 +1,13 @@
-import { Button } from "@/components/ui/button"
+'use client'
+import { Button } from '@/components/ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes'
 
 export default function Page() {
-  return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
-  )
+  const { theme, setTheme } = useTheme();
+  return <div className="flex min-h-svh p-6">
+    <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+      {theme === 'light' ? <Moon /> : <Sun />}
+    </Button>
+  </div>
 }
